a01fd67c8fb2fe82494cc335ea7662b576aa2c82
K29promax <165329046+Delmarox@users.noreply.github.com>
2024-09-27 07:57:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6AMknQhD#KNeyg2EiROD7A6qX3kwE6TIYChS68T_Zv2UXZPMKMPs",
+SESSION_ID: process.env.SESSION_ID || "6AMknQhDeyg2EiROD7A6qX3kwE6TIYChS68T_Zv2UXZPMKMPs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
